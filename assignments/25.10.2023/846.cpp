fc9f5ba78de579635638a3f5a512ce3fa14771a0
Shrey48 <141221816+Shrey48@users.noreply.github.com>
2023-11-01 13:18:14 +0530

    Create 846.cpp

---
+++ assignments/25.10.2023/846.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+   #define DPSolver                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+bool isNStraightHand(vector<int> &hand, int groupSize)
+{
+    DPSolver;
+    // we can not formate n groups each of group size.
+    if (hand.size() % groupSize != 0)
+        return false;
+
+    // create a map of int and pq ascendingly
+    map<int, priority_queue<int, vector<int>, greater<>>> mp;
+
+    // sort elements
+    sort(hand.begin(), hand.end());
+
+    // create a vis vector
+    int sz = hand.size();
+    vector<bool> vis(sz);
+
+    // iterate over hand vector and insert its elements in the mp
+    for (int i = 0; i < sz; i++)
+        mp[hand[i]].push(i);
+    
+    for (int i = 0 ; i < sz ; i ++) { 
+        // if this card is not taken into a group before.
+        if (!vis[i]) {
+            for (int count = 0; count < groupSize ; count ++) {
+                int currentCard = hand[i] + count; 
+                 if (count == groupSize - 1) {
+                    
+                    vis[mp[currentCard].top()] = true;
+                    mp[currentCard].pop();
+                    break; 
+                }
+                // if there is no consecutive card in the hand, return false; 
+                if (mp[currentCard + 1].empty())
+                    return false; 
+                else  {
+                    vis[mp[currentCard].top()] = true;
+                     mp[currentCard].pop(); 
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+};
